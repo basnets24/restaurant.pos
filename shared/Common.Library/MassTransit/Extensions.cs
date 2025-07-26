a57a2b1f1842db9258b1c3c3dbc2e5e1450723f5
basnets24 <sneha.basnet2@gmail.com>
2025-07-26 00:46:44 -0400

    added mass transit extension service

--- shared/Common.Library/MassTransit/Extensions.cs
+++ shared/Common.Library/MassTransit/Extensions.cs
@@ -1,6 +1,49 @@
+using System.Reflection;
+using Common.Library.Settings;
+using MassTransit;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
 namespace Common.Library.MassTransit;
 
 public static class Extensions
 {
+    public static IServiceCollection AddMassTransitWithRabbitMq(
+        this IServiceCollection services, 
+        Action<IRetryConfigurator>? configureRetries = null)
+    {
+        services.AddMassTransit(configure =>
+        {
+            // consumption of message
+            // any consumer classes that are in the assembly will be registered here 
+            configure.AddConsumers(Assembly.GetEntryAssembly());
+            configure.UsingRestaurantPosRabbitMq(configureRetries);
+        }); 
+        return services;
+    }
     
+    public static void UsingRestaurantPosRabbitMq(this IBusRegistrationConfigurator configure, 
+        Action<IRetryConfigurator>? configureRetries = null)
+    {
+        configure.UsingRabbitMq((context, configurator) =>
+        {
+            // context can get the configuration 
+            var configuration = context.GetRequiredService<IConfiguration>();
+            var serviceSettings = configuration.GetRequiredSection(nameof(ServiceSettings)).Get<ServiceSettings>(); 
+            var rabbitMqSettings = configuration
+                .GetRequiredSection(nameof(RabbitMqSettings)).Get<RabbitMqSettings>();
+            configurator.Host(rabbitMqSettings!.Host);
+            configurator.ConfigureEndpoints(context, new 
+                KebabCaseEndpointNameFormatter(serviceSettings!.ServiceName, false));
+            
+            if (configureRetries == null)
+            {
+                configureRetries = (retryConfigurator) =>
+                {
+                    retryConfigurator.Interval(3,  TimeSpan.FromSeconds(5));
+                };
+            }
+            configurator.UseMessageRetry(configureRetries);
+        });
+    }
 }
