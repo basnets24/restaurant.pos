52357f4299babc4d374873b86c79409b74df1b88
basnets24 <sneha.basnet2@gmail.com>
2025-08-03 22:58:54 -0400

    added cart entity

--- services/order/src/OrderService/Entities/Cart.cs
+++ services/order/src/OrderService/Entities/Cart.cs
@@ -1,6 +1,20 @@
+using Common.Library;
+
 namespace OrderService.Entities;
 
-public class Cart
+public class Cart : IEntity
 {
-    
+    public Guid Id { get; set; }
+    public Guid? TableId { get; set; }
+    public Guid? CustomerId { get; set; }
+    public List<CartItem> Items { get; set; } = new();
+    public DateTimeOffset CreatedAt { get; set; } = DateTimeOffset.UtcNow;
+}
+
+public class CartItem
+{
+    public Guid MenuItemId { get; set; }
+    public string MenuItemName { get; set; } = null!;
+    public int Quantity { get; set; }
+    public decimal UnitPrice { get; set; }
 }
