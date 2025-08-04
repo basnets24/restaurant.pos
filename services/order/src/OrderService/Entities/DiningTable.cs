7faf0449e3b137dcc89b67eec038abf10a96d6cc
basnets24 <sneha.basnet2@gmail.com>
2025-08-03 22:59:55 -0400

    added dining table entity

--- services/order/src/OrderService/Entities/DiningTable.cs
+++ services/order/src/OrderService/Entities/DiningTable.cs
@@ -1,6 +1,11 @@
+using Common.Library;
+
 namespace OrderService.Entities;
 
-public class DiningTable
+public class DiningTable : IEntity
 {
-    
+    public Guid Id { get; set; }
+    public int TableNumber { get; set; }
+    public string Status { get; set; } = "Available";
+    public Guid? ActiveCartId { get; set; }
 }
