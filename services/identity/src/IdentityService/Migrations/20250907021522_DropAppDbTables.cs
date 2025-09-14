7e5dd8bbc145cd786e54e46e024c143fb48aad23
basnets24 <sneha.basnet2@gmail.com>
2025-09-13 21:24:24 -0700

    migrations

---
+++ services/identity/src/IdentityService/Migrations/20250907021522_DropAppDbTables.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace IdentityService.Migrations
+{
+    /// <inheritdoc />
+    public partial class DropAppDbTables : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "LocationId",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropColumn(
+                name: "RestaurantId",
+                table: "AspNetUsers");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "LocationId",
+                table: "AspNetUsers",
+                type: "text",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "RestaurantId",
+                table: "AspNetUsers",
+                type: "text",
+                nullable: false,
+                defaultValue: "");
+        }
+    }
+}
---
+++ services/identity/src/IdentityService/Migrations/20250907223325_SetRidLidForSpecificUser.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace IdentityService.Migrations
+{
+    /// <inheritdoc />
+    public partial class SetRidLidForSpecificUser : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql(@"
+                UPDATE ""AspNetUsers""
+                SET ""CurrentRestaurantId"" = 'd14b89699cbd4c1d9dffb30f6809b9d4',
+                    ""CurrentLocationId""   = 'd78c6d1851ff4ed3a05a98e58ba87e63'
+                WHERE ""Id"" = '9e12066f-9a4c-4ac9-a04a-da876e260983'::uuid;
+            ");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql(@"
+                UPDATE ""AspNetUsers""
+                SET ""CurrentRestaurantId"" = NULL,
+                    ""CurrentLocationId""   = NULL
+                WHERE ""Id"" = '9e12066f-9a4c-4ac9-a04a-da876e260983'::uuid;
+            ");
+        }
+    }
+}
