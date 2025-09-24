2c4f6d0ddcd979fe165514d5c4b84b42ad72d9fc
basnets24 <sneha.basnet2@gmail.com>
2025-09-24 11:30:46 -0700

    edited config var

--- services/frontend/src/config/env.ts
+++ services/frontend/src/config/env.ts
@@ -18,12 +18,12 @@ const must = (v: string | undefined, name: string) => {
 };
 
 export const ENV = {
-    IDENTITY_URL: must(w.identityUrl ?? import.meta.env.VITE_IDENTITY_URL, "VITE_IDENTITY_URL"),
-    TENANT_URL: must((w as any).tenantUrl ?? import.meta.env.VITE_TENANT_URL, "VITE_TENANT_URL"),
-    CATALOG_URL: must(w.catalogUrl ?? import.meta.env.VITE_CATALOG_URL, "VITE_CATALOG_URL"),
-    INVENTORY_URL: must(w.inventoryUrl ?? import.meta.env.VITE_INVENTORY_URL, "VITE_INVENTORY_URL"),
-    ORDER_URL: must(w.orderUrl ?? import.meta.env.VITE_ORDER_URL, "VITE_ORDER_URL"),
-    PAYMENT_URL: must(w.paymentUrl ?? import.meta.env.VITE_PAYMENT_URL, "VITE_PAYMENT_URL"),
-    RABBITMQ_URL: must(w.rabbitmqUrl ?? import.meta.env.VITE_RABBITMQ_URL, "VITE_RABBITMQ_URL"),
+    IDENTITY_URL: must(window.IDENTITY_SERVICE_URL ?? import.meta.env.VITE_IDENTITY_URL, "VITE_IDENTITY_URL"),
+    TENANT_URL: must(window.TENANT_SERVICE_URL ?? import.meta.env.VITE_TENANT_URL, "VITE_TENANT_URL"),
+    CATALOG_URL: must(window.CATALOG_SERVICE_URL ?? import.meta.env.VITE_CATALOG_URL, "VITE_CATALOG_URL"),
+    INVENTORY_URL: must(window.INVENTORY_SERVICE_URL ?? import.meta.env.VITE_INVENTORY_URL, "VITE_INVENTORY_URL"),
+    ORDER_URL: must(window.ORDER_SERVICE_URL ?? import.meta.env.VITE_ORDER_URL, "VITE_ORDER_URL"),
+    PAYMENT_URL: must(window.PAYMENT_SERVICE_URL ?? import.meta.env.VITE_PAYMENT_URL, "VITE_PAYMENT_URL"),
+    RABBITMQ_URL: must(window.RABBITMQ_URL ?? import.meta.env.VITE_RABBITMQ_URL, "VITE_RABBITMQ_URL"),
 } as const;
 
