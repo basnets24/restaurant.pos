edaf1b1fae4b263c22cca36c051968686dc17944
Sneha Basnet <sneha.basnet@sjsu.edu>
2026-08-05 16:17:04 -0700

    Require SSL for Supabase Postgres connections

--- shared/Common.Library/PostgreSQL/PostgresSettings.cs
+++ shared/Common.Library/PostgreSQL/PostgresSettings.cs
@@ -8,6 +8,8 @@ public class PostgresSettings
     public string Username { get; set; } = null!;
     public string Password { get; set; } = null!;
 
+    // Trust Server Certificate is needed because the ASP.NET base container images'
+    // minimal CA bundle can fail to validate Supabase's cert chain otherwise.
     public string GetConnectionString() =>
-        $"Host={Host};Port={Port};Database={Database};Username={Username};Password={Password}";
+        $"Host={Host};Port={Port};Database={Database};Username={Username};Password={Password};SSL Mode=Require;Trust Server Certificate=true";
 }
