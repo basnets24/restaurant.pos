e132d512fccbc9cda5d2476d2c1f9a5f215c308a
Sneha Basnet <sneha.basnet@sjsu.edu>
2026-08-17 23:02:53 -0700

    Drop unused npm deps and identity DTO extensions
    Follow-up to a21f4a4 - these three didn't actually get staged there
    due to a git add pathspec abort.

--- services/identity/src/IdentityService/Features/Identity/Extensions/ToDtoExtensions.cs
+++ services/identity/src/IdentityService/Features/Identity/Extensions/ToDtoExtensions.cs
@@ -14,40 +14,4 @@ public static class ToDtoExtensions
             user.DisplayName,
             roles.ToList());
     }
-
-    public static UserDetailDto ToUserDetailDto(
-        this ApplicationUser user,
-        IEnumerable<string> roles)
-    {
-        var lockedOut = user.LockoutEnd.HasValue && user.LockoutEnd > DateTimeOffset.UtcNow;
-
-        return new UserDetailDto(
-            user.Id,
-            user.Email,
-            user.UserName,
-            user.DisplayName,
-            user.EmailConfirmed,
-            user.LockoutEnabled,
-            lockedOut,
-            user.AccessFailedCount,
-            user.TwoFactorEnabled,
-            user.LockoutEnd,
-            roles);
-    }
-
-    public static UserListItemDto ToUserListItemDto(
-        this ApplicationUser user,
-        IEnumerable<string> roles)
-    {
-        var lockedOut = user.LockoutEnd.HasValue && user.LockoutEnd > DateTimeOffset.UtcNow;
-
-        return new UserListItemDto(
-            user.Id,
-            user.Email,
-            user.UserName,
-            user.DisplayName,
-            user.EmailConfirmed,
-            lockedOut,
-            roles);
-    }
 }
